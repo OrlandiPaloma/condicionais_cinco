4c6b45d2f20b47cd4a3340ba47330880a51917c2
Ubiratan <palomaorlandiw@gmail.com>
2022-06-07 16:16:46 -0300

    condicionais_cinco

---
+++ condicionais_cinco.py
@@ -0,0 +1,18 @@
+# Condicionais
+# if, elif, else
+'''
+input primeiro valor
+input segundo valor
+if primeiro_valor > segundo valor
+  print o primeiro valor é maior
+else
+print o segundo valor é maior
+'''
+
+primeiro_valor = input("Digite primeiro valor: ")
+segundo_valor = input("Digite o segundo valor: ")
+if int(primeiro_valor) > int(segundo_valor):
+  print("O primeiro valor é maior")
+else:
+  print("O segundo valor é maior")
+  
